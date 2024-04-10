1e70f0c3020b11b8acbd27a14030422eec819549
prachi681 <bajpaiprachi681@gmail.com>
2024-04-10 19:54:49 +0530

    create index

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,28 @@
+// import
 const express = require('express');
-const app = express();
 const cors = require('cors');
+
+// initialize
+const app = express();
+
+const userRouter = require('./routers/userRouter');
+
+// middleware
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
+app.use(express.json());
+
+app.use('/user', userRouter);
 
 const port = 5000;
 
-app.use(cors());
-
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('response from express');
+});
+
+app.get('/add', (req, res) => {
+    res.send('response from add route');
 })
 
-app.listen(port, () => {
-    console.log('server started');
-})
+app.listen(port, () => { console.log('express server started now') });
